74d56b643021dae8c86c0d1a78a7cdddb1da355b
Dmitry Frishbuter <dmitry.frishbuter@rosberry.com>
2016-12-22 16:16:41 +0600

    Update insert method in Array+Additions

--- RSBTableViewManager/Extensions/Array+Additions.swift
+++ RSBTableViewManager/Extensions/Array+Additions.swift
@@ -16,18 +16,14 @@ extension Array {
     }
     
     mutating func insert(_ elements: [Element], at indexes: IndexSet) {
-        var index = indexes.first!
-        for element in elements {
-            if let nextIndex = indexes.integerGreaterThanOrEqualTo(index) {
-                insert(element, at: nextIndex)
-                index = nextIndex
-            }
+        for (index, intElement) in indexes.enumerated() {
+            insert(elements[index], at: intElement)
         }
     }
     
     mutating func replace(_ newElements: [Element], at indexes: IndexSet) {
-        for (index, element) in indexes.enumerated() {
-            self[element] = newElements[index]
+        for (index, intElement) in indexes.enumerated() {
+            self[intElement] = newElements[index]
         }
     }
 }
